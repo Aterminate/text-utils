7f8c3e146c69b8e2f160fa289215eb6276683f5e
ayshadeveloper <afazal.developer@gmail.com>
2022-07-13 20:59:11 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-let name= "Aysha";
+import { Navbar } from './components/Navbar';
 function App() {
   return (
       <>
-        <nav>
-           <li>Home</li>
-           <li>About</li>
-           <li>Contact</li>
-        </nav>
-        <div className="container">
-           <h1>Hello {name}</h1>
-           <p>Loreum ipsum loreum ipsum loreum ipsume loreum ipsum
-           Loreum ipsum loreum ipsum loreum ipsume loreum ipsum
-           Loreum ipsum loreum ipsum loreum ipsume loreum ipsum
-           </p>
-        </div>
+        <Navbar title="TextUtils" aboutText="About Us" />
       </>
   );
 }
